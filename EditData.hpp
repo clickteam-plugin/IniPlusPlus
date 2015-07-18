c2f22c14c7774442b75d117ee288b7622daf129d
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-07-18 12:50:28 -0500

    Begin porting properties

--- EditData.hpp
+++ EditData.hpp
@@ -12,13 +12,35 @@
 
 struct EditData
 {
-	/* MyString, MyInt, MyArray_t, MyArray
-	 * Example data
-	 */
-//	stdtstring MyString;
-//	int MyInt;
-//	typedef std::vector<float> MyArray_t;
-//	MyArray_t MyArray;
+	bool b_defaultFile;
+	bool b_ReadOnly;
+	stdtstring defaultFile;
+	int defaultFolder;
+	stdtstring defaultText;
+	bool bool_CanCreateFolders;
+	bool bool_AutoSave;
+	bool bool_stdINI;
+	bool bool_compress;
+	bool bool_encrypt;
+	stdtstring encrypt_key;
+	bool bool_newline;
+	stdtstring newline;
+	bool bool_QuoteStrings;
+	int repeatGroups;
+	char repeatItems;
+	signed char undoCount;
+	signed char redoCount;
+	bool saveRepeated;
+	bool bool_EscapeGroup;
+	bool bool_EscapeItem;
+	bool bool_EscapeValue;
+	bool bool_CaseSensitive;
+	bool globalObject;
+	bool index;
+	bool autoLoad;
+	bool subGroups;
+	bool allowEmptyGroups;
+	stdtstring globalKey;
 
 	/* <default constructor>
 	 * This is where you provide default values for
@@ -26,9 +48,32 @@ struct EditData
 	 * when your extension is first created and
 	 * default values are needed.
 	 */
-	EditData() // : MyString(_T("Hello, world!")), MyInt(1337)
+	EditData()
+	: b_defaultFile(true)
+	, b_ReadOnly()
+	, defaultFolder(2)
+	, bool_CanCreateFolders(true)
+	, bool_AutoSave(true)
+	, bool_stdINI(true)
+	, bool_compress()
+	, bool_encrypt()
+	, bool_newline()
+	, bool_QuoteStrings()
+	, repeatGroups(3)
+	, repeatItems(1)
+	, undoCount()
+	, redoCount()
+	, saveRepeated()
+	, bool_EscapeGroup()
+	, bool_EscapeItem()
+	, bool_EscapeValue()
+	, bool_CaseSensitive()
+	, globalObject()
+	, index()
+	, autoLoad()
+	, subGroups()
+	, allowEmptyGroups(1)
 	{
-		//MyArray.push_back(3.1415926f);
 	}
 
 	/* <copy constructor>
@@ -37,7 +82,36 @@ struct EditData
 	 * of the EditData class. Make sure you deep-copy
 	 * dynamically allocated memory e.g. with pointers.
 	 */
-	EditData(const EditData &from) // : MyString(from.MyString), MyInt(from.MyInt), MyArray(from.MyArray)
+	EditData(const EditData &from) // = default; //C++11
+	: b_defaultFile        (from.b_defaultFile)
+	, b_ReadOnly           (from.b_ReadOnly)
+	, defaultFile          (from.defaultFile)
+	, defaultFolder        (from.defaultFolder)
+	, defaultText          (from.defaultText)
+	, bool_CanCreateFolders(from.bool_CanCreateFolders)
+	, bool_AutoSave        (from.bool_AutoSave)
+	, bool_stdINI          (from.bool_stdINI)
+	, bool_compress        (from.bool_compress)
+	, bool_encrypt         (from.bool_encrypt)
+	, encrypt_key          (from.encrypt_key)
+	, bool_newline         (from.bool_newline)
+	, newline              (from.newline)
+	, bool_QuoteStrings    (from.bool_QuoteStrings)
+	, repeatGroups         (from.repeatGroups)
+	, repeatItems          (from.repeatItems)
+	, undoCount            (from.undoCount)
+	, redoCount            (from.redoCount)
+	, saveRepeated         (from.saveRepeated)
+	, bool_EscapeGroup     (from.bool_EscapeGroup)
+	, bool_EscapeItem      (from.bool_EscapeItem)
+	, bool_EscapeValue     (from.bool_EscapeValue)
+	, bool_CaseSensitive   (from.bool_CaseSensitive)
+	, globalObject         (from.globalObject)
+	, index                (from.index)
+	, autoLoad             (from.autoLoad)
+	, subGroups            (from.subGroups)
+	, allowEmptyGroups     (from.allowEmptyGroups)
+	, globalKey            (from.globalKey)
 	{
 		//
 	}
@@ -48,11 +122,37 @@ struct EditData
 	 * with an instance that is already
 	 * constructed.
 	 */
-	EditData &operator=(const EditData &from)
+	EditData &operator=(const EditData &from) // = default; //C++11
 	{
-//		MyString = from.MyString;
-//		MyInt = from.MyInt;
-//		MyArray = from.MyArray;
+		b_defaultFile         = from.b_defaultFile;
+		b_ReadOnly            = from.b_ReadOnly;
+		defaultFile           = from.defaultFile;
+		defaultFolder         = from.defaultFolder;
+		defaultText           = from.defaultText;
+		bool_CanCreateFolders = from.bool_CanCreateFolders;
+		bool_AutoSave         = from.bool_AutoSave;
+		bool_stdINI           = from.bool_stdINI;
+		bool_compress         = from.bool_compress;
+		bool_encrypt          = from.bool_encrypt;
+		encrypt_key           = from.encrypt_key;
+		bool_newline          = from.bool_newline;
+		newline               = from.newline;
+		bool_QuoteStrings     = from.bool_QuoteStrings;
+		repeatGroups          = from.repeatGroups;
+		repeatItems           = from.repeatItems;
+		undoCount             = from.undoCount;
+		redoCount             = from.redoCount;
+		saveRepeated          = from.saveRepeated;
+		bool_EscapeGroup      = from.bool_EscapeGroup;
+		bool_EscapeItem       = from.bool_EscapeItem;
+		bool_EscapeValue      = from.bool_EscapeValue;
+		bool_CaseSensitive    = from.bool_CaseSensitive;
+		globalObject          = from.globalObject;
+		index                 = from.index;
+		autoLoad              = from.autoLoad;
+		subGroups             = from.subGroups;
+		allowEmptyGroups      = from.allowEmptyGroups;
+		globalKey             = from.globalKey;
 	}
 
 #ifndef RUN_ONLY
@@ -64,20 +164,49 @@ struct EditData
 	 */
 	bool Serialize(mv *mV, SerializedED *&SED) const
 	{
-		//Create an instance of EDOStream, a helper class
 		EDOStream os (mV, SED);
 
-		//Write the data you need to save in binary format
-		//(you can use text format, but binary is recommended)
-//		os.write_string(MyString); //works for c-strings too
-//		os.write_value(MyInt); //only works for primitives!
-//		os.write_value(MyArray.size()); //need to know how many to load later
-//		os.write_sequence(MyArray.begin(), MyArray.end()); //works for c-style arrays too
+		os.write_value(b_defaultFile);
+		os.write_value(b_ReadOnly);
+		os.write_string(defaultFile);
+		os.write_value(defaultFolder);
+		os.write_string(defaultText);
+		os.write_value(bool_CanCreateFolders);
+		os.write_value(bool_AutoSave);
+		os.write_value(bool_stdINI);
+		os.write_value(bool_compress);
+		os.write_value(bool_encrypt);
+		os.write_string(encrypt_key);
+		os.write_value(bool_newline);
+		os.write_string(newline);
+		os.write_value(bool_QuoteStrings);
+		os.write_value(repeatGroups);
+		os.write_value(repeatItems);
+		os.write_value(undoCount);
+		os.write_value(redoCount);
+		os.write_value(saveRepeated);
+		os.write_value(bool_EscapeGroup);
+		os.write_value(bool_EscapeItem);
+		os.write_value(bool_EscapeValue);
+		os.write_value(bool_CaseSensitive);
+		os.write_value(globalObject);
+		os.write_value(index);
+		os.write_value(autoLoad);
+		os.write_value(subGroups);
+		os.write_value(allowEmptyGroups);
+		os.write_string(globalKey);
 
-		//That's it! EDOStream automatically stores the data in your extension's editdata
 		return true; //return false in the event of an error
 	}
 #endif
+
+	stdtstring upgrade(char const *old)
+	{
+		TCHAR *s = Edif::ConvertString(old);
+		stdtstring str = s;
+		Edif::FreeString(s);
+		return str;
+	}
 
 	/* <constructor>
 	 * This is the primary constructor for the
@@ -91,32 +220,130 @@ struct EditData
 	 */
 	EditData(SerializedED *SED)
 	{
-		if(SED->Header.extVersion == 0) //older version
+		if(SED->Header.extVersion == 1) //Ini++1.5
 		{
-			//code to update from an older version
+			struct V1
+			{
+				extHeader		eHeader;
+
+				bool			b_defaultFile;
+				bool			b_ReadOnly;
+				char			defaultFile[260];
+				int				defaultFolder;
+				char			defaultText[3000];
+				bool			bool_CanCreateFolders;
+				bool			bool_AutoSave;
+
+				bool			bool_stdINI;
+				bool			bool_compress;
+				bool			bool_encrypt;
+				char			encrypt_key[128];
+				
+				bool			bool_newline;
+				char			newline[10];
+				bool			bool_QuoteStrings;
+				int				repeatGroups;
+
+				char			repeatItems;
+				
+				signed char		undoCount;
+				signed char		redoCount;
+				char			placeholder;
+
+				bool			saveRepeated;
+
+				bool			bool_EscapeGroup;
+				bool			bool_EscapeItem;
+				bool			bool_EscapeValue;
+
+				bool			bool_CaseSensitive;
+
+				bool			globalObject;
+				bool			index;
+				bool			autoLoad;
+				bool			subGroups;
+				bool			allowEmptyGroups;
+
+				char			globalKey[32];
+			};
+			V1 *old = (V1 *)SED;
+
+			b_defaultFile         = old->b_defaultFile;
+			b_ReadOnly            = old->b_ReadOnly;
+			defaultFile           = upgrade(old->defaultFile);
+			defaultFolder         = old->defaultFolder;
+			defaultText           = upgrade(old->defaultText);
+			bool_CanCreateFolders = old->bool_CanCreateFolders;
+			bool_AutoSave         = old->bool_AutoSave;
+			bool_stdINI           = old->bool_stdINI;
+			bool_compress         = old->bool_compress;
+			bool_encrypt          = old->bool_encrypt;
+			encrypt_key           = upgrade(old->encrypt_key);
+			bool_newline          = old->bool_newline;
+			newline               = upgrade(old->newline);
+			bool_QuoteStrings     = old->bool_QuoteStrings;
+			repeatGroups          = old->repeatGroups;
+			repeatItems           = old->repeatItems;
+			undoCount             = old->undoCount;
+			redoCount             = old->redoCount;
+			saveRepeated          = old->saveRepeated;
+			bool_EscapeGroup      = old->bool_EscapeGroup;
+			bool_EscapeItem       = old->bool_EscapeItem;
+			bool_EscapeValue      = old->bool_EscapeValue;
+			bool_CaseSensitive    = old->bool_CaseSensitive;
+			globalObject          = old->globalObject;
+			index                 = old->index;
+			autoLoad              = old->autoLoad;
+			subGroups             = old->subGroups;
+			allowEmptyGroups      = old->allowEmptyGroups;
+			globalKey             = upgrade(old->globalKey);
 		}
-		else if(SED->Header.extVersion == 1) //current version
+		else if(SED->Header.extVersion == 2) //Ini++1.6
 		{
-			//Create an instance of EDIStream, a helper class
 			EDIStream is (SED);
-			//Read back the data in the same format that you stored it above
-//			MyString = is.read_string();
-//			MyInt = is.read_value<int>(); //need to specify the type here
-//			MyArray_t::size_type MyArray_size = is.read_value<MyArray_t::size_type>();
-//			for(MyArray_t::size_type i = 0; i < MyArray_size; ++i)
-//			{
-//				MyArray.push_back(is.read_value<MyArray_t::value_type>());
-//			}
+
+			b_defaultFile         = is.read_value<bool>();
+			b_ReadOnly            = is.read_value<bool>();
+			defaultFile           = is.read_string();
+			defaultFolder         = is.read_value<int>();
+			defaultText           = is.read_string();
+			bool_CanCreateFolders = is.read_value<bool>();
+			bool_AutoSave         = is.read_value<bool>();
+			bool_stdINI           = is.read_value<bool>();
+			bool_compress         = is.read_value<bool>();
+			bool_encrypt          = is.read_value<bool>();
+			encrypt_key           = is.read_string();
+			bool_newline          = is.read_value<bool>();
+			newline               = is.read_string();
+			bool_QuoteStrings     = is.read_value<bool>();
+			repeatGroups          = is.read_value<int>();
+			repeatItems           = is.read_value<char>();
+			undoCount             = is.read_value<signed char>();
+			redoCount             = is.read_value<signed char>();
+			saveRepeated          = is.read_value<bool>();
+			bool_EscapeGroup      = is.read_value<bool>();
+			bool_EscapeItem       = is.read_value<bool>();
+			bool_EscapeValue      = is.read_value<bool>();
+			bool_CaseSensitive    = is.read_value<bool>();
+			globalObject          = is.read_value<bool>();
+			index                 = is.read_value<bool>();
+			autoLoad              = is.read_value<bool>();
+			subGroups             = is.read_value<bool>();
+			allowEmptyGroups      = is.read_value<bool>();
+			globalKey             = is.read_string();
 		}
 		else //the version is newer than current
 		{
-			//Either try to load the data anyway assuming your
-			//future self was smart enough to keep the data in
-			//the same format with new data at the end, or
-			//make an error dialog and load some default data.
-//			MessageBox(NULL, _T("The MFA you are trying to load was saved")
-//			                 _T("with a newer version of this extension."),
-//			                 _T("Error Loading My Extension"), MB_OK);
+			*this = EditData(); //load default properties
+			MessageBox
+			(
+				NULL,
+				_T("The MFA you are trying to load was saved")
+				_T(" with a newer version of Ini++ - bad things")
+				_T(" might happen if you proceed."),
+				_T("Ini++"),
+				MB_ICONWARNING
+			);
 		}
 	}
 
@@ -126,6 +353,5 @@ struct EditData
 	 */
 	~EditData()
 	{
-		//
 	}
 };
