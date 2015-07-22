018b368e16ff4eb8c433f6dfa2795c5f5b9f84f4
Nicholas "LB" Braden <LB@LB-Stuff.com>
2015-07-21 20:39:31 -0500

    Port shift/sort/shuffle dialog + minor tweaks

--- CustomParams.cpp
+++ CustomParams.cpp
@@ -91,7 +91,7 @@ BOOL CALLBACK LoadFileSettings(HWND hDlg, UINT msgType, WPARAM wParam, LPARAM lP
  * the dialog and take input from
  * the user and store it.
  */
-void MMF2Func EditParameter(mv *mV, short ID, paramExt *p)
+void MMF2Func EditParameter(mv *mV, short ID, paramExt *p) //TODO: handle upgrade for strings
 {
 #ifndef RUN_ONLY
 	ParamInfo pi = {*p, *mV};
@@ -125,65 +125,6 @@ void MMF2Func EditParameter(mv *mV, short ID, paramExt *p)
 #endif
 }
 
-#ifndef RUN_ONLY
-
-stdtstring SettingsNames[] =
-{
-	_T("No deleting groups"),
-	_T("No deleting items"),
-	_T("No renaming groups"),
-	_T("No renaming items"),
-	_T("No changing values"),
-	_T("No refresh button"),
-	_T("No new groups"),
-	_T("No new items"),
-	_T("No auto refresh"),
-	_T("Pause application")
-};
-stdtstring SSSTag[] =
-{
-	_T("Sort (by value)"),
-	_T("Shuffle"),
-	_T("Remove first element"),
-	_T("Cycle Up"),
-	_T("Remove last element"),
-	_T("Cycle Down"),
-	_T("Reverse"),
-	_T("Swap items and values in"),
-	_T("Renumber"),
-	_T("Sort (by name)"),
-	_T("Sort (by first part)")
-};
-stdtstring SearchModeNames[] =
-{
-	_T("Disregard previous results"),
-	_T("Or"),
-	_T("And"),
-	_T("Xor")
-};
-stdtstring SearchSettingsNames[] =
-{
-	_T("Unused"),
-	_T("Only add group names to results"),
-	_T("Match any of the conditions"),
-	_T("Case sensitive")
-};
-stdtstring LoadFileStrings[] =
-{
-	_T("Load file, "),
-	_T("Change the current file path, "),
-	_T("changing the current file path, and "),
-	_T("keeping the old path, and "),
-	_T("changing the current file path if the file exists, and "),
-	_T("clearing the current data in the object."),
-	_T("keeping the current data."),
-	_T("clearing the current data in the object if the file exists."),
-	_T(" (Immedately save)"),
-	_T(" (Read Only)")
-};
-
-#endif
-
 /* GetParameterString
  * This is where you give MMF2 the display
  * string for your custom parameter. Remember,
@@ -195,14 +136,80 @@ stdtstring LoadFileStrings[] =
 void MMF2Func GetParameterString(mv *mV, short ID, paramExt const *p, LPTSTR dest, short size) //TODO: cleanup
 {
 #ifndef RUN_ONLY
+	static stdtstring const SettingsNames[] =
+	{
+		_T("No deleting groups"),
+		_T("No deleting items"),
+		_T("No renaming groups"),
+		_T("No renaming items"),
+		_T("No changing values"),
+		_T("No refresh button"),
+		_T("No new groups"),
+		_T("No new items"),
+		_T("No auto refresh"),
+		_T("Pause application")
+	};
+	static stdtstring const SSSTag[] =
+	{
+		_T("Sort (by value, "),
+		_T("Shuffle"),
+		_T("Remove first element"),
+		_T("Cycle Up"),
+		_T("Remove last element"),
+		_T("Cycle Down"),
+		_T("Reverse"),
+		_T("Swap items and values in"),
+		_T("Renumber"),
+		_T("Sort (by name, "),
+		_T("Sort (by first part, ")
+	};
+	static stdtstring const SSSType[] =
+	{
+		_T("alphanum)"),
+		_T("standard, case insensitive)"),
+		_T("standrad, case sensitive)")
+	};
+	static stdtstring const SearchModeNames[] =
+	{
+		_T("Disregard previous results"),
+		_T("Or"),
+		_T("And"),
+		_T("Xor")
+	};
+	static stdtstring const SearchSettingsNames[] =
+	{
+		_T("Unused"),
+		_T("Only add group names to results"),
+		_T("Match any of the conditions"),
+		_T("Case sensitive")
+	};
+	static stdtstring const LoadFileStrings[] =
+	{
+		_T("Load file, "),
+		_T("Change the current file path, "),
+		_T("changing the current file path, and "),
+		_T("keeping the old path, and "),
+		_T("changing the current file path if the file exists, and "),
+		_T("clearing the current data in the object."),
+		_T("keeping the current data."),
+		_T("clearing the current data in the object if the file exists."),
+		_T(" (Immedately save)"),
+		_T(" (Read Only)")
+	};
 	switch(ID - PARAM_EXTBASE)
 	{
 	case 4: //shift/sort/shuffle setup
 		{
-			std::size_t num = p->pextData[0];
-			if(num < sizeof(SSSTag)/sizeof(stdtstring))
+			std::size_t n0 = p->pextData[0];
+			std::size_t n1 = p->pextData[1];
+			if(n0 < sizeof(SSSTag)/sizeof(stdtstring) && n1 < 3 /*TODO*/)
 			{
-				_tcsncpy(dest, SSSTag[num].c_str(), size/sizeof(TCHAR));
+				stdtstring sss = SSSTag[n0];
+				if(*sss.rbegin() == ' ')
+				{
+					sss += SSSType[n1];
+				}
+				_tcsncpy(dest, sss.c_str(), size/sizeof(TCHAR));
 			}
 			else
 			{
@@ -279,7 +286,7 @@ void MMF2Func GetParameterString(mv *mV, short ID, paramExt const *p, LPTSTR des
 			}
 			_tcsncpy(dest, str.c_str(), size/sizeof(TCHAR));
 		} break;
-	case 8:
+	case 8: //load file setup
 		{
 			std::size_t num0 = 0 + p->pextData[0];
 			std::size_t num1 = 2 + p->pextData[1];
@@ -423,16 +430,119 @@ BOOL CALLBACK DialogSelector(HWND hDlg, UINT msgType, WPARAM wParam, LPARAM lPar
 }
 BOOL CALLBACK SSSSettings(HWND hDlg, UINT msgType, WPARAM wParam, LPARAM lParam)
 {
+	static stdtstring const descriptions[] =
+	{
+		_T("Rearranges the items so that they are in alphabetical order based on the items value"),
+		_T("Randomly rearranges the items in the group"),
+		_T("Removes the first item in the group"),
+		_T("Cycles the elements down, so that the current last element is made the first element"),
+		_T("Removes the last element in the group"),
+		_T("Cycles the elements up, so the current first element is made the last element"),
+		_T("Reverses the order of the group"),
+		_T("Swap every item so that its new name is its old value, and its new value is its old name"),
+		_T("Names the groups by their ordering, so the first item is called '1', the second '2', etc."),
+		_T("Rearranges the items so that they are in alphabetical order based on the items name"),
+		_T("Rearrages the items so that they are in alphabetical order based on the value before the first comma.\r\n\r\nFor instance, 'example,value' would be sorted as if its value is just 'example'")
+	};
+	std::size_t const descsize = sizeof(descriptions)/sizeof(stdtstring);
 	switch(msgType)
 	{
 	case WM_INITDIALOG:
 		{
 			SetWindowLong(hDlg, DWL_USER, lParam);
 			ParamInfo &pi = *(ParamInfo *)lParam;
+			char &n0 = pi.p.pextData[0];
+			char &n1 = pi.p.pextData[1];
 
-			//
+			if(n0 < descsize)
+			{
+				SendDlgItemMessage(hDlg, IDC_DESCRIPTIONTEXT, WM_SETTEXT, 0, LPARAM(descriptions[n0].c_str()));
+				SendDlgItemMessage(hDlg, IDC_RADIO+n0, BM_SETCHECK, BST_CHECKED, 0);
+			}
+			else
+			{
+				SendDlgItemMessage(hDlg, IDC_DESCRIPTIONTEXT, WM_SETTEXT, 0, LPARAM(_T("<invalid selection>")));
+			}
+			if(n1 < 3) //TODO: eliminate magic number
+			{
+				SendDlgItemMessage(hDlg, IDC_SORTINGRADIO1+n1, BM_SETCHECK, BST_CHECKED, 0);
+			}
+			switch(IDC_RADIO+n0)
+			{
+			case IDC_RADIO:
+			case IDC_RADIO10:
+			case IDC_RADIO11:
+				{
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO1), TRUE);
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO2), TRUE);
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO3), TRUE);
+				} break;
+			default:
+				{
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO1), FALSE);
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO2), FALSE);
+					EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO3), FALSE);
+				} break;
+			}
 
 			return TRUE;
+		} break;
+	case WM_COMMAND:
+		{
+			ParamInfo &pi = *(ParamInfo *)GetWindowLong(hDlg, DWL_USER);
+
+			switch(wmCommandID)
+			{
+			case IDOK:
+				{
+					char &n0 = pi.p.pextData[0];
+					char &n1 = pi.p.pextData[1];
+					for(n0 = 0; n0 < descsize; ++n0)
+					{
+						if(BST_CHECKED == SendDlgItemMessage(hDlg, IDC_RADIO+n0, BM_GETCHECK, 0, 0))
+						{
+							break;
+						}
+					}
+					for(n1 = 0; n1 < 3 /*TODO*/; ++n1)
+					{
+						if(BST_CHECKED == SendDlgItemMessage(hDlg, IDC_SORTINGRADIO1+n1, BM_GETCHECK, 0, 0))
+						{
+							break;
+						}
+					}
+
+					EndDialog(hDlg, TRUE);
+					return TRUE;
+				} break;
+			default:
+				{
+					for(int n = 0; n < descsize; ++n)
+					{
+						if(BST_CHECKED == SendDlgItemMessage(hDlg, IDC_RADIO+n, BM_GETCHECK, 0, 0))
+						{
+							SendDlgItemMessage(hDlg, IDC_DESCRIPTIONTEXT, WM_SETTEXT, 0, LPARAM(descriptions[n].c_str()));
+							switch(IDC_RADIO+n)
+							{
+							case IDC_RADIO:
+							case IDC_RADIO10:
+							case IDC_RADIO11:
+								{
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO1), TRUE);
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO2), TRUE);
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO3), TRUE);
+								} break;
+							default:
+								{
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO1), FALSE);
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO2), FALSE);
+									EnableWindow(GetDlgItem(hDlg, IDC_SORTINGRADIO3), FALSE);
+								} break;
+							}
+						}
+					}
+				} break;
+			}
 		} break;
 	}
 	return FALSE;
